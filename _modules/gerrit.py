233da959384796542c22e1a975fa6fde903d6607
Ales Komarek <ales.komarek@tcpcloud.eu>
2016-09-27 11:43:47 +0200

    Do not return response, nobody listens

--- _modules/gerrit.py
+++ _modules/gerrit.py
@@ -201,7 +201,6 @@ def _create_account_ssh_key(gerrit, account_id, ssh_public_key):
                  account_id)
 
     import requests
-    from pygerrit.rest import _decode_response
 
     path = 'accounts/%s/sshkeys' % (account_id)
 
@@ -211,8 +210,6 @@ def _create_account_ssh_key(gerrit, account_id, ssh_public_key):
     kwargs.update(gerrit.kwargs.copy())
 
     response = requests.put(gerrit.make_url(path), **kwargs)
-
-    return _decode_response(response)
 
 
 def _create_group_membership(gerrit, account_id, group_id):
